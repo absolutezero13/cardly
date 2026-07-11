57e71e333064aa8d10a7a4c172d375ea872daa4e
Ilkersarica <ilker@thekompanion.com>
2026-07-11 19:51:12 +0300

    feat: vercel api endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ import { Platform } from "react-native";
 const devApiHost = Platform.OS === "android" ? "10.0.2.2" : "192.168.1.26";
 
 const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL ?? `http://${devApiHost}:3000`,
+  baseURL: "https://cardly-be.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
